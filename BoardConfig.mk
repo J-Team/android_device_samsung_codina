d382ddee925a6dcdfa055fdcf719188f703761bd
Salvatore Battaglia <salvo.96bat@gmail.com>
2014-06-17 10:43:43 +0200

    Welcome to new common kernel!
    Change-Id: I979b96d3f83cc26b3d792e37a40d0e54274d1e0c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,8 +3,8 @@
 TARGET_OTA_ASSERT_DEVICE := codina,i8160,GT-I8160
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/codina
-TARGET_KERNEL_CONFIG := cyanogenmod_i8160_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/u8500
+TARGET_KERNEL_CONFIG := aosp_i8160_defconfig
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/codina/bluetooth
